2d379df0f186c67360eb930815ba80905e60a641
katyalmeida <almeidakatiane29@gmail.com>
2024-07-21 16:15:13 -0300

    style: global style settings

---
+++ src/styles/globalstyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const globalStyle = createGlobalStyle `
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Nunito", sans-serif;
+    font-optical-sizing: auto;
+    // font-weight: <weight>;
+    font-style: normal;
+
+}
+
+`;
+
+export default globalStyle
